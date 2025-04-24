3ff10603ec186cbb6082f9bff4a8952af2fb914d
woodyspivey <woodyspivey@gmail.com>
2025-04-24 18:37:58 -0400

    add java

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,6 +1,4 @@
 local config = {
-  cmd = { vim.fn.expand '~/.local/share/nvim/mason/bin/jdtls' },
+  cmd = { vim.fn.expand '~/.local/share/nvim/mason/bin/jdtls', vim.fn.expand '~./local/share/nvim/mason/packages/jdtls/lombok.jar' },
   root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
 }
-
-require('jdtls').start_or_attach(config)
